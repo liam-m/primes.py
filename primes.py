5460502ffa00e341f22c0686dc2c4c1ea68b95fd
Liam <a>
2013-08-23 00:39:20 +0100

    Fixes #2, _posOf and _numAt functions, combined res and primes in primesUpTo
    * Issue #2 should now be fixed. Some extra work may be needed with the
    logic (e.g. lines 63-68) but at least it works. Essentially the issue
    was partly due to indentation in lines 38-39 and partly due to working
    out primes above sqrt(x) when they were already passed in primes
    * Private _posOf and _numAt functions to calculate position of number
    in lst and number position in lst refers to
    * Got rid of res in primesUpTo - using primes (parameter) instead as
    they are essentially the same thing and this should save some memory

--- primes.py
+++ primes.py
@@ -1,6 +1,14 @@
 from binarySearch import binarySearch
 from math import sqrt, log
 
+def _posOf(num, offset = 3):
+    # Position of number in lst
+    return int((num - offset) / 2)
+
+def _numAt(pos, offset = 3):
+    # Number at position in lst e.g. lst[0] refers to 3, lst[2] refers to 7
+    return pos * 2 + offset
+
 def primesUpTo(x, primes = []):
     """
     Implementation of Sieve of Eratosthenes
@@ -9,37 +17,59 @@ def primesUpTo(x, primes = []):
 
     Can pass in a list of known primes to decrease execution time
     """
+
+    if primes == [2]:
+        primes = [] # Not particularly useful as we only list odd numbers
     
     if x <= 1: #  No primes <= 1
         return []
     
     else: # Some primes
-        lst = [True] * int((x-1)/2) # All odd numbers initially assumed prime. Each element refers to an odd number, starting at 3, i.e. lst[0] refers to 3, lst[4] refers to 11
+        # All odd numbers initially assumed prime. Each element in lst refers to an odd number, starting at 3, i.e. lst[0] refers to 3, lst[4] refers to 11
+        lst = [True] * int((x-1)/2)
         
         if primes: # Some primes have already been worked out
+            
             if primes[-1] >= x: # Enough primes have already been worked out
+                
                 for index in range(len(primes)): # Return primes up to that number
                     if primes[index] > x:
                         return primes[:index]
-                    return primes # All primes are there
+                else: # All primes are there
+                    return primes
+                    
             else: # Not all primes up to x have been worked out
-                res = primes[:] 
-                for num in res[1:]: # Skipping 2, go through the primes
-                    for i in range(int((num**2 - 3)/2), len(lst), num): # Mark multiples of the prime as not prime
+                
+                for num in primes[1:]: # Skipping 2, go through the primes
+                    for i in range(_posOf(num**2), len(lst), num): # Mark multiples of the prime above its square as not prime
                         lst[i] = False
-                start = int((res[-1] - 3) / 2)+1 # Start at the position of the last prime + 1
+                start = _posOf(primes[-1])+1 # Start at the position of the last prime + 1
                         
         else: # No primes worked out
-            res = [2] # Only going to work out odd primes so remember 2 is prime
+            primes = [2] # Only going to work out odd primes so remember 2 is prime
             start = 0 # Start at 0
-        for index in range(start, int((sqrt(x)-3)/2+1)): # Mark multiples of prime <= square root as not prime 
+
+        for index in range(start, _posOf(int(sqrt(x)))+1): # Mark multiples of prime <= square root as not prime
             if lst[index]: # If number at index is prime
-                num = index*2 + 3 # Number index refers to is the index*2 + 3 i.e. lst[0] refers to 3, lst[2] refers to 7
-                res.append(num) # It is prime, so add it to res
-                for i in range(int((num**2 - 3)/2), len(lst), num): # Mark multiples of the prime as not prime
+                num = _numAt(index) # Number index refers to
+                primes.append(num) # It is prime, so add it to list of primes
+                for i in range(_posOf(num**2), len(lst), num): # Mark multiples of the prime above its square as not prime
                     lst[i] = False
-        res += [index*2+3 for index in range(int((sqrt(x)-3)/2+1), len(lst)) if lst[index]] # Get the primes above the square root
-        return res
+
+        posOfSqrt = _posOf(int(sqrt(x)))
+        if start < posOfSqrt:
+            start = posOfSqrt+1
+
+        # This part is because 2 and 3 are found as primes <= square root in range 4-25
+        if x < 25: 
+            if x == 3: # Only one where start should be 0 (referring to 3)
+                start = 0
+            else: # Start would be 0 (referring to 3), has to be at least 1 (referring to 5)
+                start = 1
+
+        primes += [_numAt(index) for index in range(start, len(lst)) if lst[index]] # Get the primes above the square root
+        
+        return primes
 
 def isPrime(x, primes = []):
     """
@@ -73,12 +103,11 @@ def nPrimes(n, primes = []):
     Can pass in a list of known primes to decrease execution time
     """
     if n >= 8602:
-        upperBound = n*log(n) + n*(log(log(n)) - 0.9385)
+        upperBound = int(n*log(n) + n*(log(log(n)) - 0.9385))
     elif n >= 6:
-        upperBound = n * (log(n) + log(log(n)))
+        upperBound = int(n * (log(n) + log(log(n))))
     else:
         upperBound = 13
-    upperBound = int(upperBound)
     primes = primesUpTo(upperBound, primes)
     return primes[:n]
 
