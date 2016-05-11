afd96eb85f6a308469a626b1db2bdaf5302ecd81
liam-m <liam-m@users.noreply.github.com>
2016-05-12 00:39:59 +0100

    Lucas probable prime test
    Implementation of the strong Lucas probable prime test, from
    https://github.com/smllmn/baillie-psw/ with some modifications.
    
    Closes #10

--- primes.py
+++ primes.py
@@ -246,6 +246,100 @@ def _miller_rabin_2(num):
 
     return False
 
+def _jacobi_symbol(a, n):
+    """
+    Calculate the Jacobi symbol (a/n)
+    """
+    if n == 1:
+        return 1
+    elif a == 0:
+        return 0
+    elif a == 1:
+        return 1
+    elif a == 2:
+        if n % 8 in [3, 5]:
+            return -1
+        elif n % 8 in [1, 7]:
+            return 1
+    elif a < 0:
+        return (-1)**((n-1)//2) * _jacobi_symbol(-1*a, n)
+
+    if a % 2 == 0:
+        return _jacobi_symbol(2, n) * _jacobi_symbol(a // 2, n)
+    elif a % n != a:
+        return _jacobi_symbol(a % n, n)
+    else:
+        if a % 4 == n % 4 == 3:
+            return -1 * _jacobi_symbol(n, a)
+        else:
+            return _jacobi_symbol(n, a)
+
+def _D_chooser(num):
+    """
+    Choose a D value suitable for the Baillie-PSW test
+    """
+    D = 5
+    while _jacobi_symbol(D, num) != -1:
+        D += 2 if D > 0 else -2
+        D *= -1
+    return D
+
+def _U_V_subscript(k, n, U, V, P, Q, D):
+    k, n, U, V, P, Q, D = map(int, (k, n, U, V, P, Q, D))
+    digits = list(map(int, str(bin(k))[2:]))
+    subscript = 1
+    for digit in digits[1:]:
+        U, V = U*V % n, (pow(V, 2, n) - 2*pow(Q, subscript, n)) % n
+        subscript *= 2
+        if digit == 1:
+            if not (P*U + V) & 1:
+                if not (D*U + P*V) & 1:
+                    U, V = (P*U + V) >> 1, (D*U + P*V) >> 1
+                else:
+                    U, V = (P*U + V) >> 1, (D*U + P*V + n) >> 1
+            elif not (D*U + P*V) & 1:
+                U, V = (P*U + V + n) >> 1, (D*U + P*V) >> 1
+            else:
+                U, V = (P*U + V + n) >> 1, (D*U + P*V + n) >> 1
+            subscript += 1
+            U, V = U % n, V % n
+    return U, V
+
+def _lucas_pp(num):
+    """
+    Perform the Lucas probable prime test
+
+    Returns True if num is probably prime, False if n is composite
+    """
+    if num == 2:
+        return True
+
+    D = _D_chooser(num)
+    P = 1
+    Q = (1-D)//4
+    U, V = _U_V_subscript(num+1, num, 1, P, P, Q, D)
+
+    if U != 0:
+        return False
+
+    d = num + 1
+    s = 0
+    while not d & 1:
+        d = d >> 1
+        s += 1
+
+    U, V = _U_V_subscript(num+1, num, 1, P, P, Q, D)
+
+    if U == 0:
+        return True
+
+    for r in range(s):
+        U, V = (U*V) % num, (pow(V, 2, num) - 2*pow(Q, d*(2**r), num)) % num
+        if V == 0:
+            return True
+
+    return False
+
 def is_prime(num, primes=None):
     """
     Returns True if num is a prime number, False if it is not
@@ -266,11 +360,7 @@ def is_prime(num, primes=None):
     if not _trial_division(num, [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47]):
         return False
 
-    if not _miller_rabin_2(num):
-        return False
-
-    # Skip first 15 primes tried earlier
-    return _trial_division(num, primes_up_to(int(num ** 0.5), primes)[15:])
+    return _miller_rabin_2(num) and _lucas_pp(num)
 
 def n_primes(num, primes=None):
     """
